95e82d1bf1cc15dc724f9a81ac500eae70bb6137
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-07-12 10:10:30 +0300

    Base Exception Abstract Class created

---
+++ RecipeApp.Core/Application/Wrappers/Exception/Base/aBaseException.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using RecipeApp.Core.Wrappers.Response;
+
+namespace RecipeApp.Core.Wrappers.Exception.Base
+{
+
+    public abstract class aBaseException
+    {
+        public HttpStatusCode StatusCode { get; init; }
+        public string Message {get;init;}
+        public bool Success {get;init;}
+
+
+        public BaseResponse HandleException()
+        {
+            return new BaseResponse()
+            {
+                Status = this.StatusCode,
+                Message = this.Message,
+                Success = this.Success
+            };
+        }
+        
+    }
+
+}
