58a503be71a4345ec82c77a7636aef472e350a5f
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-07-12 10:10:13 +0300

    BaseResponse Wrapper Created

---
+++ RecipeApp.Core/Application/Wrappers/Response/BaseResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace RecipeApp.Core.Wrappers.Response
+{
+
+    public class BaseResponse : BaseResponse<object>
+    {
+        public BaseResponse()
+        {
+            Status = HttpStatusCode.OK;
+            Success = true;
+        }
+    }
+
+    public class BaseResponse<T>
+    {
+        public HttpStatusCode Status { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public bool Success { get; set; }
+    }
+
+}
