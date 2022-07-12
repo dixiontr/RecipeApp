5d73f3a3e7d2200e4b9cb648e3ab8b9d59f29a1d
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-07-12 10:18:05 +0300

    ExceptionHandlingMiddleware.cs created

---
+++ RecipeApp.Core/Application/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using RecipeApp.Core.Wrappers.Exception;
+using Serilog;
+
+namespace RecipeApp.Core.Middlewares
+{
+
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        private ILogger _logger;
+
+        public ExceptionHandlingMiddleware(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message,ex);
+                await HandleErrorFromMiddleware(context);
+            }
+        }
+        
+        private async Task HandleErrorFromMiddleware(HttpContext context)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = (int)HttpStatusCode.OK;
+            var result = JsonSerializer.Serialize(new UnhandledException().HandleException());
+            await response.WriteAsync(result);
+            
+        }
+    }
+
+}
---
+++ RecipeApp.Core/Application/Wrappers/Exception/UnhandledException.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using RecipeApp.Core.Wrappers.Exception.Base;
+
+namespace RecipeApp.Core.Wrappers.Exception
+{
+
+    public class UnhandledException : aBaseException
+    {
+        public UnhandledException()
+        {
+            StatusCode = HttpStatusCode.InternalServerError;
+            Message = "Beklenmedik bir hata oluştu";
+            Success = false;
+        }
+        
+    }
+
+}
